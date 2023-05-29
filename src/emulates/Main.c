d9ff4d6e175e1115f520aa6da45ea2312d9bae0c
Weijun <weijun.huang22@imperial.ac.uk>
2023-05-29 17:45:46 +0100

    modify type of memory into int. general purpose register into list

--- src/emulates/Main.c
+++ src/emulates/Main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 int const MAX_MEMORY = 2100000; // 2^21  2097152
-long long memory[MAX_MEMORY];
+int memory[MAX_MEMORY];
 
 struct PSTATE
 {
@@ -10,19 +10,18 @@ struct PSTATE
 
 struct Registers {
     // int R0 to R30 for 31 genreal purpose registers
-    long long R0, R1, R2, R3, R4, R5, R6, R7, R8, R9,
-        R10, R11, R12, R13, R14, R15, R16, R17, R18, R19,
-        R20, R21, R22, R23, R24, R25, R26, R27, R28, R29,
-        R30;
+    long long general[31];
     // Special registers
     long long PC, ZR, SP;
     // processor state register
     struct PSTATE pstate;
 };
 
-int main(int argc, char *argv[]) {
-    // struct Registers registers;
-    // registers.pstate.N = 0;
-    // printf("%d", registers.pstate.N);
-    return 0;
-}
+// int main(int argc, char *argv[]) {
+//     struct Registers registers;
+//     struct Registers* registers_ptr = &registers;
+//     registers_ptr->R1 = 0;
+//     // registers.pstate.N = 0;
+//     // printf("%d", registers.pstate.N);
+//     return 0;
+// }
---
+++ src/emulates/input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+
+
+
+
+
+
+// int main(int argc, char const *argv[])
+// {
+//     return 0;
+// }
