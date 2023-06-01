50106762726134f0b70c689966afef508f1c6372
my1322 <my1322.ic.ac.uk>
2023-06-01 01:34:45 +0100

    finnished DPI

--- src/emulates/pipeline/input.h
+++ src/emulates/pipeline/input.h
@@ -1,4 +1,4 @@
-#include "../memory.h"
-extern void decode(int* memory, struct Registers registers, int instruction);
+#include "../Util.h"
+extern void decode(int* memory, struct Registers* registers, int instruction);
 extern void get_instruction(char* buffer, int file_size, int* memory);
 extern void readfile(char* filename, int* memory);
