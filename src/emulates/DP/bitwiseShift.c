0c948bd7ca067f7b4f5ae25a6aed61ef30999da2
yj1722 <yj1722@imperial.ac.uk>
2023-06-05 16:14:56 +0100

    fix the problem ror2

--- src/emulates/DP/bitwiseShift.c
+++ src/emulates/DP/bitwiseShift.c
@@ -13,6 +13,7 @@ long long logicalShiftLeft(long long value, int shiftAmount, bool is64Bit) {
 long long logicalShiftRight(long long value, int shiftAmount, bool is64Bit) {
     if (is64Bit) {
         return (unsigned long long)(value) >> shiftAmount; //shift right with long long (64-bit)
+        
     } else {
         return (unsigned int)(value) >> shiftAmount; //shift right with int (32-bit)
     }
@@ -32,7 +33,10 @@ long long rotateRight(long long value, int shiftAmount, bool is64Bit) {
     if (is64Bit) {
         const int numBits = 64;
         shiftAmount %= numBits;// Ensure shiftAmount is within the range of valid bits
-        return (value >> shiftAmount) | (value << (numBits - shiftAmount)); 
+        long long left = value << (numBits - shiftAmount);
+        long long right = logicalShiftRight(value, shiftAmount, is64Bit); // Left shift value by (numBits - shiftAmount) bits
+        long long result = right | left;
+        return result;
         // Right shift value by shiftAmount bits and left shift value by (numBits - shiftAmount) bits
         // Then perform bitwise OR between the shifted values to get the final result
 
@@ -44,3 +48,22 @@ long long rotateRight(long long value, int shiftAmount, bool is64Bit) {
         // Right shift value by shiftAmount bits and left shift value by (numBits - shiftAmount) bits . Then perform bitwise OR between the shifted values to get the final result
     }
 }
+// long long rotateRight(long long value, int shiftAmount, bool is64Bit) {
+//     int bitSize = is64Bit ? 64 : 32;  // Choose bit size based on is64Bit
+//     long long result = 0;
+//     if (shiftAmount >= bitSize) {  // Ensure shiftAmount is less than bitSize
+//         shiftAmount %= bitSize;
+//     }
+
+//     long long right = value >> shiftAmount;
+//     long long left = value << (bitSize - shiftAmount);
+
+//     // If it is 32 bits, mask left to remove unwanted high bits
+//     if (!is64Bit) {
+//         left &= 0xFFFFFFFF;
+//     }
+//     result = right | left;
+
+//     return result;
+
+// }
