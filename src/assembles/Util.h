c136680d37c553235e53e97db8475aa149b1934e
Weijun <weijun.huang22@imperial.ac.uk>
2023-06-06 19:56:59 +0100

    basicformat of util

--- src/assembles/Util.h
+++ src/assembles/Util.h
@@ -1,13 +1,19 @@
 extern char* register_to_bin(char* in);
-
-struct symbol_table
-{
+extern void copy_bit(unsigned int* in, unsigned int change, int start, int end);
+// lsl #0
+struct node{
     char* name;
     int address;
 };
+struct symbol_table
+{
+    struct node* node;
+    // next
+    struct symbol_table* next;
+};
+extern struct symbol_table* symbol_table_init();
+extern void symbol_table_add(struct symbol_table* table, char* name, int address);
+extern int symbol_table_get(struct symbol_table* table, char* name);
 
-struct symbol_table* symbol_table_init();
-void symbol_table_add(struct symbol_table* table, char* name, int address);
-int symbol_table_get(struct symbol_table* table, char* name);
-void symbol_table_free(struct symbol_table* table);
-
+// check in 32 bit or 64 bit;
+extern char check_bit(char* in, int bit);
