c10932b5b1504a11a300f32f352b2bd9c1a89508
Your Name <peter@.localdomain>
2023-06-07 04:13:15 +0100

    pull

---
+++ src/emulates/single_data_transfer/loadl.h
@@ -0,0 +1,3 @@
+#include "../Util.h"
+#include "../inputformat.h"
+extern void LoadLiteral(char* memory, struct Registers* regs, struct loadliteral l);
---
+++ src/emulates/single_data_transfer/sdt.h
@@ -0,0 +1,4 @@
+#include "../Util.h"
+#include "../inputformat.h"
+extern long long getRegisterValue(int n, struct Registers* regs);
+extern void SingleDataTransfer(char* memory, struct Registers* regs, struct sdtp s);
