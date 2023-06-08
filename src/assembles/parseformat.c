25b1c82c3b69cedfc55a41208c3910badae7f321
Weijun <weijun.huang22@imperial.ac.uk>
2023-06-08 18:03:23 +0100

    change oprand name into opcode

--- src/assembles/parseformat.c
+++ src/assembles/parseformat.c
@@ -1,60 +1,60 @@
 #include "parseformat.h"
-struct add_sub_immediate add_sub_immdiate_init(char *operend, char *rd, char *rn, char *imm, char *shift)
+struct add_sub_immediate add_sub_immdiate_init(char *opcode, char *rd, char *rn, char *imm, char *shift)
 {
-    struct add_sub_immediate divide = {operend, rd, rn, imm, shift};
+    struct add_sub_immediate divide = {opcode, rd, rn, imm, shift};
     return divide;
 }
 
-struct add_sub_register add_sub_register_init(char *operend, char *rd, char *rn, char *rm, char *shift)
+struct add_sub_register add_sub_register_init(char *opcode, char *rd, char *rn, char *rm, char *shift)
 {
-    struct add_sub_register divide = {operend, rd, rn, rm, shift};
+    struct add_sub_register divide = {opcode, rd, rn, rm, shift};
     return divide;
 }
 
-struct logical logical_init(char *operend, char *rd, char *rn, char *rm, char *shift)
+struct logical logical_init(char *opcode, char *rd, char *rn, char *rm, char *shift)
 {
-    struct logical divide = {operend, rd, rn, rm, shift};
+    struct logical divide = {opcode, rd, rn, rm, shift};
     return divide;
 }
 
-struct move_wide move_wide_init(char *operend, char *rd, char *imm, char *shift)
+struct move_wide move_wide_init(char *opcode, char *rd, char *imm, char *shift)
 {
-    struct move_wide divide = {operend, rd, imm, shift};
+    struct move_wide divide = {opcode, rd, imm, shift};
     return divide;
 }
 
-struct multiply multiply_init(char *operend, char *rd, char *rn, char *rm, char *ra)
+struct multiply multiply_init(char *opcode, char *rd, char *rn, char *rm, char *ra)
 {
-    struct multiply divide = {operend, rd, rn, rm, ra};
+    struct multiply divide = {opcode, rd, rn, rm, ra};
     return divide;
 }
 
-struct branch branch_init(char *operend, char *literal)
+struct branch branch_init(char *opcode, char *literal)
 {
-    struct branch divide = {operend, literal};
+    struct branch divide = {opcode, literal};
     return divide;
 }
 
-struct load_store load_store_init(char *operend, char *rt, char *xn, char *simm)
+struct load_store load_store_init(char *opcode, char *rt, char *xn, char *simm)
 {
-    struct load_store divide = {operend, rt, xn, simm};
+    struct load_store divide = {opcode, rt, xn, simm};
     return divide;
 }
 
-struct load_store_shift load_store_shift_init(char *operend, char *rt, char *xn, char *rm, char *shift)
+struct load_store_shift load_store_shift_init(char *opcode, char *rt, char *xn, char *rm, char *shift)
 {
-    struct load_store_shift divide = {operend, rt, xn, rm, shift};
+    struct load_store_shift divide = {opcode, rt, xn, rm, shift};
     return divide;
 }
 
-struct load_store_literal load_store_literal_init(char *operend, char *literal)
+struct load_store_literal load_store_literal_init(char *opcode, char *literal)
 {
-    struct load_store_literal divide = {operend, literal};
+    struct load_store_literal divide = {opcode, literal};
     return divide;
 }
 
-struct constant constant_init(char *operend, char *simm)
+struct constant constant_init(char *opcode, char *simm)
 {
-    struct constant divide = {operend, simm};
+    struct constant divide = {opcode, simm};
     return divide;
 }
