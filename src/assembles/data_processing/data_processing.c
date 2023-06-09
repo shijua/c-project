76edb49757988c22c3c9b35b42e9bdfeee96f0bd
Weijun <weijun.huang22@imperial.ac.uk>
2023-06-09 19:50:37 +0100

    fix data processing

--- src/assembles/data_processing/data_processing.c
+++ src/assembles/data_processing/data_processing.c
@@ -41,12 +41,14 @@ extern void tokenise_add_sub_immediate (unsigned int* instr , struct add_sub_imm
     copy_arithm_opc (instr , divide.opcode); //opc
     copy_bit (instr , (check_bit (divide.rd)) , 31 , 31);//sf
     copy_bit (instr , 4 , 26 , 28);//op0
+    copy_bit (instr , 2 , 23 , 25);//opi
     copy_bit (instr , register_to_bin(divide.rd) , 0 , 4);//rd
     divide.shift == NULL ? copy_bit (instr , 0 , 22 , 22) : copy_bit (instr , 1 , 22 , 22);//sh
     //divide.imm + 3 is for eliminating the prefix "#0x" of imm
     //strtol is turns a string representing hex to a int.
-    copy_bit (instr , strtol(divide.imm + 3, NULL, 16) , 16 , 21); //imm12
+    copy_bit (instr , strtol(divide.imm + 3, NULL, 16) , 10 , 21); //imm12
     copy_bit (instr , register_to_bin(divide.rn) , 5 , 9); //rn
+
 }
 
 extern void tokenise_add_sub_register (unsigned int* instr , struct add_sub_register divide){
@@ -112,6 +114,7 @@ extern void tokenise_move_wide (unsigned int* instr , struct move_wide divide){
     if(!strcmp (divide.opcode, "movk")){
         copy_bit (instr , 3 , 29 , 30);
     }
+    copy_bit (instr , 2 , 23 , 25);//opi
     copy_bit (instr , (check_bit (divide.rd)) , 31 , 31);//sf
     copy_bit (instr , 4 , 26 , 28);//op0
     copy_bit (instr , register_to_bin(divide.rd) , 0 , 4);//rd
